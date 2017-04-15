c86675c80e31019619d45d24bb07b79b9c4d5515
Sreedevi Krishnan <sreedevik29@gmail.com>
2017-04-15 01:18:28 -0400

    Fix spacing and add comments

--- js/scripts.js
+++ js/scripts.js
@@ -1,50 +1,30 @@
 $(document).ready(function(){
 
-		var a = Math.floor((Math.random()) * 10);
-		var b = Math.floor((Math.random()) * 10);
-		$("#value-a").text(a);
-		$("#value-b").text(b);
+  // Generate random numbers
+  var a = Math.floor((Math.random()) * 10);
+  var b = Math.floor((Math.random()) * 10);
 
-		$("#check-btn").click(function(){
-			var sum = a + b;
-			var answer = $("#answer").val();
-				if (sum == answer)
-				{
-					$("#result").text("You are correct!");
-					a = Math.floor((Math.random()) * 10);
-					b = Math.floor((Math.random()) * 10);
-					$("#value-a").text(a);
-					$("#value-b").text(b);
-					$("#answer").val("");
-					$("#answer").focus();
-				}
-				else{
-					$("#result").text("You are incorrect!");
-				}
-			});
-		});
+  // Assign random numbers to value fields
+  $("#value-a").text(a);
+  $("#value-b").text(b);
 
+  // Handle check click
+  $("#check-btn").click(function(){
+    var sum = a + b;
+    var answer = $("#answer").val();
+      if (sum == answer) {
+        $("#result").text("You are correct!");
+        a = Math.floor((Math.random()) * 10);
+        b = Math.floor((Math.random()) * 10);
+        $("#value-a").text(a);
+        $("#value-b").text(b);
+        $("#answer").val("");
+        $("#answer").focus();
+      } else {
+        $("#result").text("You are incorrect!");
+      }
+  });
 
-
-
-
-// 		var a = Math.floor((Math.random()) * 10);
-// 		var b = Math.floor((Math.random()) * 10);
-// 		$("#value-a").html(a);
-// 		$("#value-b").html(b);
-// 		var s = parseInt(a) + parseInt(b);
-	
-// 		$("#check-btn").click(function(){
-// 		var r = "#result";
-// 			if (s === r)
-// 			{
-// 				document.write("You are correct!")
-// 			}
-// 			else{
-// 				document.write("You are incorrect!")
-// 			}
-// 		});
-// });
-
+});
 
 
