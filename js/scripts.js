214a8c398f75cefe6dd18091ca64859aac6fa387
Sreedevi Krishnan <sreedevik29@gmail.com>
2017-04-16 22:46:48 -0400

    Major updates to UI and game logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,78 @@
 $(document).ready(function(){
 
+  // Dom Selectors
+  var answerArea = $("#answer-area");
+  var question = $("#question");
+
   // Generate random numbers
   var a = Math.floor((Math.random()) * 10);
   var b = Math.floor((Math.random()) * 10);
 
   // Assign random numbers to value fields
-  $("#value-a").text(a);
-  $("#value-b").text(b);
+  question.text(a + " + " + b);
 
-  // Handle check click
-  $("#check-btn").click(function(){
+  // Event Handlers: Handle check click
+  $("#check-btn").on('click', function(){
+
     var sum = a + b;
-    var answer = $("#answer").val();
-      if (sum == answer) {
-        $("#result").text("You are correct!");
-        a = Math.floor((Math.random()) * 10);
-        b = Math.floor((Math.random()) * 10);
-        $("#value-a").text(a);
-        $("#value-b").text(b);
-        $("#answer").val("");
-        $("#answer").focus();
-      } else {
-        $("#result").text("You are incorrect!");
-      }
+    var answer = parseInt(answerArea.text());
+
+    if (sum == answer) {
+      $("#result").text("You are correct!");
+      a = Math.floor((Math.random()) * 10);
+      b = Math.floor((Math.random()) * 10);
+      question.text(a + " + " + b);
+      answerArea.text("");
+    } else {
+      $("#result").text("You are incorrect, try again!");
+      question.text(a + " + " + b);
+      answerArea.text("");
+    }
+
+  });
+
+  // Dom Click Handlers
+  $("#num-7").on('click', function(){
+    answerArea.text(answerArea.text() + '7');
+  });
+  
+  $("#num-8").on('click', function(){
+    answerArea.text(answerArea.text() + '8');
+  });
+
+  $("#num-9").on('click', function(){
+    answerArea.text(answerArea.text() + '9');
+  });
+
+  $("#num-4").on('click', function(){
+    answerArea.text(answerArea.text() + '4');
+  });
+
+  $("#num-5").on('click', function(){
+    answerArea.text(answerArea.text() + '5');
+  });
+
+  $("#num-6").on('click', function(){
+    answerArea.text(answerArea.text() + '6');
+  });
+
+  $("#num-1").on('click', function(){
+    answerArea.text(answerArea.text() + '1');
+  });
+
+  $("#num-2").on('click', function(){
+    answerArea.text(answerArea.text() + '2');
+  });
+
+  $("#num-3").on('click', function(){
+    answerArea.text(answerArea.text() + '3');
+  });
+
+  $("#num-0").on('click', function(){
+    answerArea.text(answerArea.text() + '0');
   });
 
 });
+
 
 
